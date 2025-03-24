a55b090d7616f1e1415f796fe8758adfeaad50e7
elknu <elknumber1@yandex.com>
2025-03-25 01:20:18 +0300

    User model and basic reviews logic are created

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,48 @@
+class ReviewsController < ApplicationController
+
+  before_action :set_review!, only: %i[show edit update destroy]
+
+  def show
+    render json: @review
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new review_params
+    if @review.save
+      render json: "Successfully updated"
+    else
+      render json: "Error, check your data and try again"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update review_params
+      render json: "Successfully updated"
+    else
+      render json: "Error, check your data and try again"
+    end
+  end
+
+  def destroy
+    @review.destroy
+    render json: "Your review is successfully deleted"
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :rating, :price, :description, :producer, :abv, :user_id)
+  end
+
+  def set_review!
+    @review = Review.find params[:id]
+  end
+
+end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   has_many_attached :images
+  belongs_to :user
 
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
-  
+  validates :abv, numericality: true, format: { with: /\A\d+(\.\d{1})?\z/, message: "Значение должно быть числом с максимум одним знаком после запятой" }
+  validates :producer, presence: true, length: { minimum: 1 }
+  validates :name, presence: true, length: { minimum: 1 }
 end
